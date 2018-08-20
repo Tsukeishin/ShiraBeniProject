bbe1eabf64cfdbc41421011f0761a5f08719dabd
DESKTOP-6CKN28J\aster <astertataricus246@gmail.com>
2018-08-21 00:37:45 +0900

    エンターキー入れた

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,7 @@
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	MessageBox(NULL, "Hello World!", "メッセージ", MB_OK);
+	MessageBox(NULL, "Hello World??", "メッセージ", MB_OK);
 
 	return 0;
 }
