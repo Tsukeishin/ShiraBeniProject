59adf96249366c40622a0cecf3b644224ad44644
Tsukeishin <38748315+Tsukeishin@users.noreply.github.com>
2018-08-22 02:55:45 +0900

    再構築

---
+++ Library/Fade.h
@@ -0,0 +1,50 @@
+//=============================================================================
+//
+// フェード処理 <Fade.h>
+//
+//=============================================================================
+#ifndef __FADE_INCLUDE_H__
+#define __FADE_INCLUDE_H__
+
+#include "LinkLibrary.h"
+
+/* フェードの状態 */
+typedef enum
+{
+	FADE_NONE = 0,		// 何もない状態
+	FADE_IN,			// フェードイン処理
+	FADE_OUT,			// フェードアウト処理
+	FADE_MAX
+} FADE;
+
+
+class CSFade
+{
+	static VERTEX_2D	Vertex[NUM_VERTEX];
+	static D3DXCOLOR	Color;
+	static GAMESCENE	GameScene;
+	static FADE			Fade;
+	static float		Speed;
+
+public:
+	static void Update(void);
+	static void Draw(void);
+
+	static void MakeVertex(void);
+	static void SetColor(D3DCOLOR col);
+	static void SetColor(void);
+	static void SetFade(float spd);
+	static void SetFade(float spd, D3DCOLOR col);
+	static void SetFade(GAMESCENE scene);
+	static void SetFade(GAMESCENE scene, float spd);
+	static void SetFade(FADE fade, GAMESCENE scene, float spd);
+	static FADE GetFade(void);
+
+private:
+
+
+};
+
+
+
+#endif
---
+++ Library/Sound.h
@@ -0,0 +1,46 @@
+//=============================================================================
+//
+// サウンド処理 <Sound.h>
+//
+//=============================================================================
+#ifndef ___SOUND_INCLUDE_H___
+#define ___SOUND_INCLUDE_H___
+
+#include <windows.h>
+#include <tchar.h>
+#include <dsound.h>
+#include <mmsystem.h>
+
+
+enum
+{	// 再生用フラグ
+	E_DS8_FLAG_NONE,
+	E_DS8_FLAG_LOOP,	// DSBPLAY_LOOPING=1
+	E_DS8_FLAG_MAX
+};
+
+
+typedef class DirectSound
+{
+	static IDirectSound8 *DirectSoundInterface;	// サウンドインターフェース
+
+	LPDIRECTSOUNDBUFFER8 SoundBuffer;
+
+public:
+	DirectSound();
+
+	static HRESULT Init(HWND hWnd);
+	static void    Uninit(void);
+
+	LPDIRECTSOUNDBUFFER8 LoadSound(const char *soundFile);
+	void Play(int flag);
+	void Stop();
+	void Volume(LONG volume);
+	bool CheckPlaying();
+	void Release();
+
+}DSound;
+
+
+
+#endif
