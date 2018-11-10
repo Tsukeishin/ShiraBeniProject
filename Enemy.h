f44d0221eeadb2c46e19e44c002f43b5108b669b
DESKTOP-6CKN28J\aster <astertataricus246@gmail.com>
2018-11-10 23:31:03 +0900

    エラー解除のため

---
+++ Enemy.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Library/ObjectBase3D.h"
+
+typedef struct Capacity_Enemy
+{
+	_ObjectBase3D ObjectBase_Enemy;
+	int Number_Enemy;
+	int HP_Enemy;
+
+};
+
+class Original_Enemy
+{
+private:
+
+	Capacity_Enemy Capacity_Enemy;
+
+public:
+
+
+
+};
+
